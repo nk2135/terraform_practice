12acafe393822e2ebfa6c0d71dee01b38f7e30be
Nilesh <nk2135@gmail.com>
2024-07-27 20:20:52 +0530

    updated main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   subnet {
     name = "subnet1"
     address_prefix = "10.0.1.0/24"
-  }
+    }
   subnet {
     name = "subnet2"
     address_prefix = "10.0.2.0/24"
